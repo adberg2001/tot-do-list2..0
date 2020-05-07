dafd2f1ff9dae0a9cc8272bacd3f5b1c28fe46f2
adberg2001 <aaaadyl.disk.erg@gmail.com>
2020-05-07 16:19:27 +0600

    refactor desing

--- src/api.js
+++ src/api.js
@@ -1,87 +1,5 @@
 const someUrl = `http://localhost:3000`;
 
-// const makeList = (row) => {
-//     const list = document.getElementById('row');
-//     const liCont = setAtr('div', 'class', 'li-cont');
-//     const li = setAtr('li', 'class', 'li', row.text);
-//     if (!row.done) {
-//         li.style.textDecoration = 'line-through';
-//     }
-//
-//     const remasterBtn = setAtr('button', 'class', 'remasterBtn', 'Редактировать');
-//     const doneBtn = setAtr('button', 'class', "doneBtn", row.done ? 'Сделано' : "Не сделано");
-//     const descBtn = setAtr('button', 'class', 'descBtn', 'Описание');
-//     const liDelete = setAtr('button', 'class', "li-delete", 'Удалить');
-//
-//     doneBtn.onclick = () => {
-//         row.done === false ?
-//             api.doneTask(row.id, true) :
-//             api.doneTask(row.id, false);
-//     };
-//     liDelete.onclick = () => {
-//         api.deleteTask(row.id)
-//     };
-//     descBtn.onclick = () => {
-//         if (!descBtn.hasAttribute('data-value')) {
-//             api.openDesc(descBtn, row.id);
-//             descBtn.setAttribute('data-value', 'true');
-//         } else {
-//             descBtn.removeAttribute('data-value');
-//             descBtn.parentNode.lastChild.classList.toggle('pNone')
-//         }
-//     };
-//     remasterBtn.onclick = () => {
-//         const input = setAtr('input', 'placeholder', liCont.firstChild.textContent);
-//         input.classList.add('createInput');
-//         li.textContent = '';
-//         liCont.replaceChild(input, liCont.firstChild);
-//         remasterBtn.disabled = true;
-//         input.onchange = () => {
-//             api.editTask(row.id, input.value);
-//             remasterBtn.disabled = false;
-//         }
-//     };
-//
-//     liCont.appendChild(li);
-//     liCont.appendChild(remasterBtn);
-//     liCont.appendChild(descBtn);
-//     liCont.appendChild(doneBtn);
-//     liCont.appendChild(liDelete);
-//     list.appendChild(liCont);
-// };
-
-const getList = () => {
-    fetch(`${someUrl}/list`)
-        .then(response => {
-            if (!response.ok) throw new Error("Невозможно загрузить данные, пожалуйтса, перезагрузите стр!");
-            return response.json()
-        })
-        .then(data => {
-
-        })
-        .catch((err) => {
-            editError(err);
-        });
-};
-
-getList();
-
-const createList = () => {
-    fetch(`${someUrl}/list`)
-        .then(response => {
-            if (!response.ok) throw new Error("Невозможно загрузить данные, пожалуйтса, перезагрузите стр!");
-            return response.json()
-        })
-        .then(data => {
-            const list = document.getElementById('row');
-            list.innerHTML = '';
-            data.forEach((d) => array.push(d));
-        })
-        .catch((err) => {
-            editError(err);
-        });
-};
-
 const error = document.querySelector('.error');
 const editError = (err) => {
     console.log(err);
@@ -91,26 +9,32 @@ const editError = (err) => {
 };
 
 export default {
-    createTask: (text, done, desc) => {
-        fetch(`${someUrl}/add`, {
+    createLst: () => {
+        return fetch(`${someUrl}/list`)
+            .then( response => {
+                if (!response.ok) throw  new Error('Ошибка загрузки списка заметок');
+                return response.json()})
+    },
+    createTask: (text, done, desc, color) => {
+        return fetch(`${someUrl}/add`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 text: text,
                 done: done,
-                desc: desc
+                desc: desc,
+                color: color
             })
         })
             .then(response => {
                 if (!response.ok) throw new Error("Ошибка, вас взломали!")
             })
-            .then(() => createList())
             .catch((err) => {
                 editError(err)
             })
     },
-    editTask: (id, text) => {
-        fetch(`${someUrl}/edit/${id}`, {
+    editHeadOfList: (id, text) => {
+        return fetch(`${someUrl}/edit/${id}`, {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({text: text}),
@@ -118,45 +42,55 @@ export default {
             .then(response => {
                 if (!response.ok) throw new Error('Ошибка с редактированием!')
             })
-            .then(() => createList())
+            .catch((err) => {
+                editError(err)
+            })
+    },
+    editDescOfList: (id, text) => {
+        return fetch(`${someUrl}/edit/${id}`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({text: text}),
+        })
+            .then(response => {
+                if (!response.ok) throw new Error('Ошибка с редактированием!')
+            })
             .catch((err) => {
                 editError(err)
             })
     },
     doneTask: (id, text) => {
-        fetch(`${someUrl}/edit/${id}`, {
+        return fetch(`${someUrl}/edit/${id}`, {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({done: text}),
         }).then(response => {
             if (!response.ok) throw new Error("Ошибка с выполнением задачи!")
         })
-            .then(() => createList())
             .catch((err) => {
                 editError(err)
             })
     },
     deleteTask: (id) => {
-        fetch(`${someUrl}/delete/${id}`, {
+        return fetch(`${someUrl}/delete/${id}`, {
             method: 'DELETE',
             headers: {'Content-Type': 'application/json'}
         }).then(response => {
             if (!response.ok) throw new Error("Ошибка с удалением!")
-        })
-            .then(() => createList())
-            .catch((err) => {
+        }).catch((err) => {
                 editError(err)
             })
     },
-    openDesc: (descBtn, id) => {
-        const p = document.createElement('p');
-        fetch(`${someUrl}/list/${id}`)
-            .then(response => response.json())
-            .then(data => {
-                p.textContent = data.desc;
-                descBtn.parentNode.appendChild(p);
-            });
-    },
-    startCreateList : () => createList(),
-    getArray: () => getList()
+    changeBg: (color, id) => {
+        return fetch(`${someUrl}/edit/${id}`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({color: color}),
+        }).then(response => {
+            if (!response.ok) throw new Error("Ошибка с выполнением задачи!")
+        })
+            .catch((err) => {
+                editError(err)
+            })
+    }
 }
--- src/index.js
+++ src/index.js
@@ -2,72 +2,207 @@ import api from './api.js';
 
 const makeList = (row) => {
     const list = document.getElementById('row');
+    const p = setAtr('p', null, null, row.desc);
     const liCont = setAtr('div', 'class', 'li-cont');
+
+    const liDelete = setAtr('button', 'class', "li-delete");
+    liDelete.style.backgroundImage = "url('img/delete.svg')";
+
+    liCont.style.backgroundColor = row.color;
     const li = setAtr('li', 'class', 'li', row.text);
     if (!row.done) {
         li.style.textDecoration = 'line-through';
     }
 
-    const remasterBtn = setAtr('button', 'class', 'remasterBtn', 'Редактировать');
-    const doneBtn = setAtr('button', 'class', "doneBtn", row.done ? 'Сделано' : "Не сделано");
-    const descBtn = setAtr('button', 'class', 'descBtn', 'Описание');
-    const liDelete = setAtr('button', 'class', "li-delete", 'Удалить');
+    const doneBtn = setAtr('button', 'class', "doneBtn");
+    doneBtn.innerHTML = ` 
+    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path id='${row.id}' d="M15 0C6.71652 0 0 6.71652 0 15C0 23.2835 6.71652 30 15 30C23.2835 30 30 23.2835 30 15C30 6.71652 23.2835 0 15 0ZM21.4788 10.1016L14.4275 19.8783C14.3289 20.0159 14.199 20.128 14.0485 20.2053C13.8979 20.2826 13.7312 20.323 13.5619 20.323C13.3927 20.323 13.2259 20.2826 13.0754 20.2053C12.9249 20.128 12.795 20.0159 12.6964 19.8783L8.5212 14.0926C8.39397 13.9152 8.5212 13.6674 8.73884 13.6674H10.3092C10.6507 13.6674 10.9754 13.8315 11.1763 14.1127L13.5603 17.4208L18.8237 10.1217C19.0246 9.84375 19.346 9.67634 19.6908 9.67634H21.2612C21.4788 9.67634 21.606 9.92411 21.4788 10.1016Z" fill="current-color"/>
+</svg>
+    `;
+    row.done ?
+        doneBtn.style.backgroundImage = "url('../img/icon.svg')" :
+        doneBtn.style.backgroundImage = "url('../img/greenIcon.svg')";
+
+    const colorCont = setAtr('div', 'class', 'color-cont');
+    const whiteLabel = setAtr('label', 'class', 'color-label');
+    const pinkLabel = setAtr('label', 'class', 'color-label');
+    const grayLabel = setAtr('label', 'class', 'color-label');
+    const blueLabel = setAtr('label', 'class', 'color-label');
+    const yellowLabel = setAtr('label', 'class', 'color-label');
+
+    const whiteRadio = setAtr('input', 'type', 'radio');
+    whiteRadio.setAttribute('name', row.id);
+
+    const pinkRadio = setAtr('input', 'type', 'radio');
+    pinkRadio.setAttribute('name', row.id);
+
+    const grayRadio = setAtr('input', 'type', 'radio');
+    grayRadio.setAttribute('name', row.id);
+
+    const blueRadio = setAtr('input', 'type', 'radio');
+    blueRadio.setAttribute('name', row.id);
+
+    const yellowRadio = setAtr('input', 'type', 'radio');
+    yellowRadio.setAttribute('name', row.id);
+
+
+    const whiteCircle = setAtr('div', 'class', 'color-circle white-color-btn');
+    const pinkCircle = setAtr('div', 'class', 'color-circle pink-color-btn');
+    const grayCircle = setAtr('div', 'class', 'color-circle gray-color-btn');
+    const blueCircle = setAtr('div', 'class', 'color-circle blue-color-btn');
+    const yellowCircle = setAtr('div', 'class', 'color-circle yellow-color-btn');
+
+    if (row.color === '#FFFBE3')setBorderToInput(whiteCircle);
+    if (row.color === "#FFD2D2")setBorderToInput(pinkCircle);
+    if (row.color === "#D3D2FF")setBorderToInput(grayCircle);
+    if (row.color === "#D7FDD1")setBorderToInput(blueCircle);
+    if (row.color === "#F8E1B6")setBorderToInput(yellowCircle);
+
+    whiteRadio.addEventListener('change', () => {
+        api.changeBg('#FFFBE3', row.id);
+        getList();
+    });
+    pinkRadio.addEventListener('change', () => {
+        api.changeBg('#FFD2D2', row.id);
+        getList();
+    });
+    grayRadio.addEventListener('change', () => {
+        api.changeBg('#D3D2FF', row.id);
+        getList();
+    });
+    blueRadio.addEventListener('change', () => {
+        api.changeBg('#D7FDD1', row.id);
+        getList();
+    });
+    yellowRadio.addEventListener('change', () => {
+        api.changeBg('#F8E1B6', row.id);
+        getList();
+    });
+
 
     doneBtn.onclick = () => {
         row.done === false ?
             api.doneTask(row.id, true) :
             api.doneTask(row.id, false);
+        getList();
     };
     liDelete.onclick = () => {
-        api.deleteTask(row.id)
+        api.deleteTask(row.id);
+        getList();
     };
-    descBtn.onclick = () => {
-        if (!descBtn.hasAttribute('data-value')) {
-            api.openDesc(descBtn, row.id);
-            descBtn.setAttribute('data-value', 'true');
-        } else {
-            descBtn.removeAttribute('data-value');
-            descBtn.parentNode.lastChild.classList.toggle('pNone')
-        }
-    };
-    remasterBtn.onclick = () => {
+    li.onclick = () => {
         const input = setAtr('input', 'placeholder', liCont.firstChild.textContent);
         input.classList.add('createInput');
         li.textContent = '';
         liCont.replaceChild(input, liCont.firstChild);
-        remasterBtn.disabled = true;
         input.onchange = () => {
-            api.editTask(row.id, input.value);
-            remasterBtn.disabled = false;
-        }
+            api.editHeadOfList(row.id, input.value)
+                .then(() => getList());
+        };
+    };
+    p.onclick = () => {
+        const input = setAtr('textarea', 'placeholder', liCont.children[2].textContent);
+        input.classList.add('descInput');
+        p.textContent = '';
+        liCont.replaceChild(input, liCont.children[2]);
+        input.onchange = () => {
+            api.editDescOfList(row.id, input.value)
+                .then(() => getList());
+        };
     };
 
+    whiteLabel.appendChild(whiteRadio);
+    whiteLabel.appendChild(whiteCircle);
+    pinkLabel.appendChild(pinkRadio);
+    pinkLabel.appendChild(pinkCircle);
+    grayLabel.appendChild(grayRadio);
+    grayLabel.appendChild(grayCircle);
+    blueLabel.appendChild(blueRadio);
+    blueLabel.appendChild(blueCircle);
+    yellowLabel.appendChild(yellowRadio);
+    yellowLabel.appendChild(yellowCircle);
+
+    colorCont.appendChild(whiteLabel);
+    colorCont.appendChild(pinkLabel);
+    colorCont.appendChild(grayLabel);
+    colorCont.appendChild(blueLabel);
+    colorCont.appendChild(yellowLabel);
+
     liCont.appendChild(li);
-    liCont.appendChild(remasterBtn);
-    liCont.appendChild(descBtn);
     liCont.appendChild(doneBtn);
+    liCont.appendChild(p);
+    liCont.appendChild(colorCont);
     liCont.appendChild(liDelete);
     list.appendChild(liCont);
 };
 
-api.getList();
+const getList = () => {
+    const list = document.getElementById('row');
 
-const setAtr =  (tag, atr, atrName, text) => {
+    list.innerHTML = '';
+    api.createLst()
+        .then(list => {
+            let openedValue = 0;
+            let closedValue = 0;
+            list.forEach((l) => {
+                makeList(l);
+                l.done ? openedValue++ : closedValue++;
+            });
+            getTotalDone(openedValue, closedValue);
+        });
+};
+
+getList();
+
+const setAtr = (tag, atr, atrName, text) => {
     const el = document.createElement(tag);
     el.setAttribute(atr, atrName);
     el.textContent = text;
     return el;
 };
 
+const setBorderToInput = (input) => {
+    input.setAttribute('id', 'checkedInput')
+};
+
+const getTotalDone = (open, close) => {
+    const rangeCont = document.getElementsByClassName('range');
+    const opened = document.getElementById('opened');
+    const closed = document.getElementById('closed');
+    const openRinger = document.getElementById('openRinger');
+    const closeRinger = document.getElementById('closeRinger');
+    let total = open + close;
+
+    if (open === 0 && close === 0){
+        rangeCont.innerHTML = ' ';
+    }else{
+        opened.textContent = open;
+        closed.textContent = close;
+    }
+
+    openRinger.style.width = `${(open * 100) / total}%`;
+    closeRinger.style.width = `${(close * 100) / total}%`;
+};
+
 const headerRow = document.getElementById('header-row');
 const textArea = document.getElementById('text');
 const addBtn = document.getElementById('input-btn');
+const colorInput = document.querySelectorAll('.color-input');
+let variableColor = '#FFFBE3';
+
+colorInput.forEach((input) => {
+    input.addEventListener('change', () => {
+        variableColor = `${input.value}`;
+    });
+});
 
 addBtn.addEventListener('click', () => {
     if (headerRow.value && textArea.value) {
-        api.createTask(headerRow.value, true, textArea.value)
+        api.createTask(headerRow.value, true, textArea.value, variableColor);
     } else {
         headerRow.setAttribute('placeholder', 'Не оставляйте поле пустым!');
         textArea.setAttribute('placeholder', 'Не оставляйте поле пустым!');
     }
+    getList()
 });
